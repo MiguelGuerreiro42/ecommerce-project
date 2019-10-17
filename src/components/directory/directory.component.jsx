ea62623cf530bc230545a6e219ca7ab5080e03d3
MiguelGuerreiro42 <miguelpachiega@hotmail.com>
2019-10-17 17:26:00 -0300

    implementacao arquitetura

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MenuItem from './../menu-item/menu-item.component'
+
+import './directory.component.scss';
+
+const Directory = () => (
+    <div className='directory-menu'>
+    <MenuItem/>
+    </div>
+);
