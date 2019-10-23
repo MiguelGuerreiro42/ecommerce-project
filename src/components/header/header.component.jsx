1f261eaca7fad63deefd28e39538866a2c905060
MiguelGuerreiro42 <miguelpachiega@hotmail.com>
2019-10-23 10:09:31 -0300

    Implantaçao do REDUX no login

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,29 +1,39 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import {auth} from './../../firebase/firebase.utils'
+import { auth } from "./../../firebase/firebase.utils";
 
-import {ReactComponent as Logo} from "../../assets/crown.svg";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
 
-const Header = ({currentUser}) => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
     </Link>
 
     <div className="options">
-      <Link to="/shop" className="option">COMPRA</Link>
-      <Link to="/" className="option">SOBRE</Link>
-      {
-        currentUser ?
-        (<div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>)
-        :
-        (<Link to="/signInUp">SIGN IN</Link>)
-      }
+      <Link to="/shop" className="option">
+        COMPRA
+      </Link>
+      <Link to="/" className="option">
+        SOBRE
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link to="/signInUp">SIGN IN</Link>
+      )}
     </div>
   </div>
 );
 
-export default Header;
+const mapStatetoProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStatetoProps)(Header);
